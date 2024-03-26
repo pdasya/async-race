3d4934091fcb039eac1d7888741464866f750a4d
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-03-26 11:50:42 +0900

    feat: implement Page class

---
+++ async-race/src/core/templates/page.ts
@@ -0,0 +1,21 @@
+export default class Page {
+    container: HTMLElement;
+
+    static TextObject = {};
+
+    constructor(id: string) {
+        this.container = document.createElement('div');
+        this.container.className = 'main-container';
+        this.container.id = id;
+    }
+
+    createHeaderTitle(text: string) {
+        const headerTitle = document.createElement('h1');
+        headerTitle.textContent = text;
+        return headerTitle;
+    }
+
+    render() {
+        return this.container;
+    }
+}
--- async-race/src/index.ts
+++
@@ -1,3 +0,0 @@
-const x = 4;
-
-console.log(x);
