4e92e2a76cf0d1843aca8e8f50fec57676679c03
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-03-27 13:28:44 +0900

    feat: add EventObserver class

---
+++ async-race/src/core/eventObservers/eventObserver.ts
@@ -0,0 +1,19 @@
+export default class EventObserver<Listener> {
+    observers: Array<(T: Listener) => void>;
+
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(observer: (T: Listener) => void): void {
+        this.observers.push(observer);
+    }
+
+    unsubscribe(observer: (T: Listener) => void): void {
+        this.observers = this.observers.filter((subscriber) => subscriber !== observer);
+    }
+
+    notify(data: Listener): void {
+        this.observers.forEach((subscriber) => subscriber(data));
+    }
+}
