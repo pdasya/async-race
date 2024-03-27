49e4a7acb72c24a8ca9cce92af36c0152419724f
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-03-27 16:31:27 +0900

    feat: update Store class

--- async-race/src/core/store/store.ts
+++ async-race/src/core/store/store.ts
@@ -1,18 +1,44 @@
-// eslint-disable-next-line import/no-unresolved, import/extensions
+// eslint-disable-next-line import/no-unresolved
 import EventObserver from '@core/eventObservers/eventObserver';
+import type { ICar } from '@core/types/interfaces';
 
+type TStore = ICar | HTMLDivElement | HTMLInputElement;
 export default class Store {
-    static store: Map<string, EventObserver<unknown>> = new Map();
+    static event: Map<string, EventObserver<unknown>> = new Map();
 
-    static getStore(): Map<string, EventObserver<unknown>> {
-        return Store.store;
+    static store: Map<string, TStore> = new Map();
+
+    static currentPage: number = 1;
+
+    static getCurrentPage(): number {
+        return Store.currentPage;
     }
 
-    static addToStore(key: string, value: EventObserver<unknown>) {
+    static setCurrentPage(page: number): void {
+        Store.currentPage = page;
+    }
+
+    static addToStore(key: string, value: TStore) {
         Store.store.set(key, value);
     }
 
-    static remove(key: string): void {
+    static removeFromStore(key: string) {
         Store.store.delete(key);
+    }
+
+    static getFromStore(key: string): TStore | undefined {
+        return Store.store.get(key);
+    }
+
+    static getEvent(): Map<string, EventObserver<unknown>> {
+        return Store.event;
+    }
+
+    static addToEvent(key: string, value: EventObserver<unknown>) {
+        Store.event.set(key, value);
+    }
+
+    static removeEvent(key: string): void {
+        Store.event.delete(key);
     }
 }
